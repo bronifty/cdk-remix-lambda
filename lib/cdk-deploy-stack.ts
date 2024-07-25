4ab9263bdbb7128d24297721da3c9f4bff5a1070
bronifty <bronifty@gmail.com>
2024-07-25 11:48:58 -0400

    this

--- lib/cdk-deploy-stack.ts
+++ lib/cdk-deploy-stack.ts
@@ -1,16 +1,34 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import { root } from "../utils/root";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkDeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambda = new cdk.aws_lambda.Function(this, "remix-lambda", {
+      runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+      handler: "lambda.handler",
+      code: cdk.aws_lambda.Code.fromAsset(`${__dirname}/deploy`),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDeployQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a Function URL for the Lambda
+    const functionUrl = lambda.addFunctionUrl({
+      authType: cdk.aws_lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [cdk.aws_lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+      },
+    });
+
+    // Export the Function URL as a stack output
+    new cdk.CfnOutput(this, "LambdaFunctionUrl", {
+      value: functionUrl.url,
+      description: "URL of the Lambda function",
+      exportName: "RemixLambdaFunctionUrl",
+    });
   }
 }
